0ceba8833744d429643d865644337f4de316e933
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-10-30 16:45:34 -0600

    adicion de creacion proyecto de escritorio

---
+++ Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Proyecto_final_TallerBasesDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.playwindow.proyecto_final_tallerbasesdatos;
+
+/**
+ *
+ * @author daniel
+ */
+public class Proyecto_final_TallerBasesDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
