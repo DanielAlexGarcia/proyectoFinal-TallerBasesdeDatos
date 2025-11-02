b8deb1fc8c27f4e776b1c21a672d099a8ba5ff29
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-11-02 13:36:18 -0600

    adicion correcta de modelo cita

--- Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Cita.java
+++ Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Cita.java
@@ -9,5 +9,21 @@ package com.playwindow.proyecto_final_tallerbasesdatos.Modelos;
  * @author daniel
  */
 public class Cita {
+    private int ID;
+    private int IDPaciente;
+    private int IDPersonal;
+    private String fecha;
+    private String Hora;
+    private String motivo;
+    private String estado;
     
+    public Cita(int id, int idPaciente, int idPersonal, String fecha, String hora, String motivo,
+            String estado){
+        this.ID = id;
+        this.IDPaciente = idPaciente;
+        this.IDPersonal = idPersonal;
+        this.fecha = fecha;
+        this.Hora = hora;
+        this.estado = estado;
+    }
 }
