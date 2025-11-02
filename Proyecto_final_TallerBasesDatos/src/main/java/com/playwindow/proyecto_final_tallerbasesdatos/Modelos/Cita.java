3f276eb0d643805b5c6dcb97e988ec17e544b647
Daniel Alexander <daniel.gt@tecjerez.edu.mx>
2025-11-02 13:37:14 -0600

    guardado de coambios pendientes

--- Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Cita.java
+++ Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Cita.java
@@ -17,6 +17,7 @@ public class Cita {
     private String motivo;
     private String estado;
     
+    //constructor con ID
     public Cita(int id, int idPaciente, int idPersonal, String fecha, String hora, String motivo,
             String estado){
         this.ID = id;
@@ -26,4 +27,71 @@ public class Cita {
         this.Hora = hora;
         this.estado = estado;
     }
+    
+    //Constructor sin ID
+    public Cita(int idPaciente, int idPersonal, String fecha, String hora, String motivo,
+            String estado){
+        this.IDPaciente = idPaciente;
+        this.IDPersonal = idPersonal;
+        this.fecha = fecha;
+        this.Hora = hora;
+        this.estado = estado;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getIDPaciente() {
+        return IDPaciente;
+    }
+
+    public void setIDPaciente(int IDPaciente) {
+        this.IDPaciente = IDPaciente;
+    }
+
+    public int getIDPersonal() {
+        return IDPersonal;
+    }
+
+    public void setIDPersonal(int IDPersonal) {
+        this.IDPersonal = IDPersonal;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return Hora;
+    }
+
+    public void setHora(String Hora) {
+        this.Hora = Hora;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
 }
--- Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Personal.java
+++ Proyecto_final_TallerBasesDatos/src/main/java/com/playwindow/proyecto_final_tallerbasesdatos/Modelos/Personal.java
@@ -9,5 +9,82 @@ package com.playwindow.proyecto_final_tallerbasesdatos.Modelos;
  * @author daniel
  */
 public class Personal {
+    private int ID;
+    private int DNIPersona;
+    private String rol;
+    private String departamento;
+    private String especialidad;
+    private float salario;
+    
+    // contructor con ID
+    
+    public Personal(int id, int dniPersona, String Rol, String Departamento, String Especialidad,
+            float Salario){
+        this.ID = id;
+        this.DNIPersona = dniPersona;
+        this.rol = Rol;
+        this.departamento = Departamento;
+        this.especialidad = Especialidad;
+        this.salario = Salario;
+    }
+    
+    // constructor sin Id
+    
+    public Personal(int dniPersona, String Rol, String Departamento, String Especialidad,
+            float Salario){
+        this.DNIPersona = dniPersona;
+        this.rol = Rol;
+        this.departamento = Departamento;
+        this.especialidad = Especialidad;
+        this.salario = Salario;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getDNIPersona() {
+        return DNIPersona;
+    }
+
+    public void setDNIPersona(int DNIPersona) {
+        this.DNIPersona = DNIPersona;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
     
 }
